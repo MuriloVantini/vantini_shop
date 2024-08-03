331131da4fc684e257d4b625de004e1ba1b18ec3
Murilo Vantini <m.henrique.vantini18@gmail.com>
2024-08-03 13:29:18 -0300

    feat: criar modelo de usuário

---
+++ lib/src/models/user_model.dart
@@ -0,0 +1,69 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
+class UserModel {
+  int id;
+  String email;
+  String name;
+  String? emailVerifiedAt;
+  String createdAt;
+  String updatedAt;
+  bool isAdmin;
+  UserModel({
+    required this.id,
+    required this.email,
+    required this.name,
+    this.emailVerifiedAt,
+    required this.createdAt,
+    required this.updatedAt,
+    required this.isAdmin,
+  });
+
+  UserModel copyWith({
+    int? id,
+    String? email,
+    String? name,
+    String? emailVerifiedAt,
+    String? createdAt,
+    String? updatedAt,
+    bool? isAdmin,
+  }) {
+    return UserModel(
+      id: id ?? this.id,
+      email: email ?? this.email,
+      name: name ?? this.name,
+      emailVerifiedAt: emailVerifiedAt ?? this.emailVerifiedAt,
+      createdAt: createdAt ?? this.createdAt,
+      updatedAt: updatedAt ?? this.updatedAt,
+      isAdmin: isAdmin ?? this.isAdmin,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'id': id,
+      'email': email,
+      'name': name,
+      'emailVerifiedAt': emailVerifiedAt,
+      'createdAt': createdAt,
+      'updatedAt': updatedAt,
+      'isAdmin': isAdmin,
+    };
+  }
+
+  factory UserModel.fromMap(Map<String, dynamic> map) {
+    return UserModel(
+      id: map['id'] as int,
+      email: map['email'] as String,
+      name: map['name'] as String,
+      emailVerifiedAt: map['emailVerifiedAt'] != null ? map['emailVerifiedAt'] as String : null,
+      createdAt: map['createdAt'] as String,
+      updatedAt: map['updatedAt'] as String,
+      isAdmin: map['isAdmin'] as bool,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory UserModel.fromJson(String source) => UserModel.fromMap(json.decode(source) as Map<String, dynamic>);
+}
