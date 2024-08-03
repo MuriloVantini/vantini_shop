341043287e3825f381e26e8768eff3b922b310fc
Murilo Vantini <m.henrique.vantini18@gmail.com>
2024-08-03 13:29:49 -0300

    feat: adicionar login dinâmico e com tratamento de exceptions estilizada

--- lib/src/screens/auth/login.dart
+++ lib/src/screens/auth/login.dart
@@ -1,4 +1,7 @@
+import 'package:email_validator/email_validator.dart';
 import 'package:flutter/material.dart';
+import 'package:vantini_shop/api/repositories/auth_repository.dart';
+import 'package:vantini_shop/api/stores/auth_store.dart';
 import 'package:vantini_shop/src/settings/settings_view.dart';
 
 class LoginPage extends StatefulWidget {
@@ -12,6 +15,7 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
+  AuthStore authStore = AuthStore(repository: AuthRepository());
 
   @override
   void initState() {
@@ -43,37 +47,92 @@ class _LoginPageState extends State<LoginPage> {
         padding: const EdgeInsets.symmetric(horizontal: 20),
         child: Form(
           key: formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Center(child: Text('Login', style: Theme.of(context).textTheme.titleLarge, textAlign: TextAlign.center)),
-              const SizedBox(height: 120),
+          child: AnimatedBuilder(
+            animation: Listenable.merge([authStore.stateToLogin, authStore.isLoading, authStore.erro]),
+            builder: (context, child) {
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Center(child: Text('Login', style: Theme.of(context).textTheme.titleLarge, textAlign: TextAlign.center)),
+                  const SizedBox(height: 120),
 
-              //Email
-              TextFormField(
-                controller: emailController,
-                decoration: const InputDecoration(labelText: 'Email'),
-              ),
-
-              const SizedBox(height: 40),
-
-              //Senha
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
+                  //Email
                   TextFormField(
-                    controller: passwordController,
-                    decoration: const InputDecoration(labelText: 'Senha'),
+                    controller: emailController,
+                    decoration: const InputDecoration(labelText: 'Email'),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Informe um email.';
+                      }
+                      if (!EmailValidator.validate(value)) {
+                        return 'Email inválido.';
+                      }
+                      if (authStore.erro.value.isNotEmpty) {
+                        return authStore.erro.value;
+                      }
+                      return null;
+                    },
                   ),
-                  const Padding(
-                    padding: EdgeInsets.only(left: 8, top: 5),
-                    child: Text('Esqueceu a senha?', style: TextStyle(color: Colors.white38)),
+
+                  const SizedBox(height: 40),
+
+                  //Senha
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      TextFormField(
+                        controller: passwordController,
+                        decoration: const InputDecoration(labelText: 'Senha'),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Informe uma senha.';
+                          }
+                          return null;
+                        },
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 8, top: 20, right: 8),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text('Esqueceu a senha?', style: Theme.of(context).textTheme.bodySmall),
+                            Text(
+                              'Registre-se',
+                              style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                                    color: Theme.of(context).textTheme.titleLarge!.color,
+                                    decoration: TextDecoration.underline,
+                                    decorationThickness: 0.5,
+                                  ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
                   ),
+                  const SizedBox(height: 40),
+                  authStore.isLoading.value
+                      ? const Center(child: CircularProgressIndicator.adaptive())
+                      : TextButton(
+                          onPressed: () async {
+                            if (formKey.currentState!.validate()) {
+                              await authStore.login(
+                                {"email": emailController.text.trim(), "password": passwordController.text.trim()},
+                              ).whenComplete(() {
+                                if (authStore.erro.value.isNotEmpty) {
+                                  formKey.currentState!.validate();
+                                  authStore.erro.value = '';
+                                }
+                                if (authStore.stateToLogin.value.isNotEmpty) {
+                                  Navigator.popAndPushNamed(context, '/home');
+                                }
+                              });
+                            }
+                          },
+                          child: const Text('Entrar'))
                 ],
-              ),
-              const SizedBox(height: 40),
-              TextButton.icon(onPressed: () {}, label: const Text('Login'), icon: const Icon(Icons.login))
-            ],
+              );
+            },
           ),
         ),
       ),
